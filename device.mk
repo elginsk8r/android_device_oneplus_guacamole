242e58e41f9c7b6483c1956c647420133ca718c6
Nick Reuter <nreuter85@gmail.com>
2016-11-10 15:56:09 -0600

    guacamole: initial setup to support aosp
    Change-Id: I17da1754471e38c8bd8e2c278ba595d5acaa8f4d

--- device.mk
+++ device.mk
@@ -3,16 +3,15 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/oneplus/sm8150-common/common.mk)
-
-# Get non-open-source specific aspects
-$(call inherit-product, vendor/oneplus/guacamole/guacamole-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
+
+# AAPT
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -25,6 +24,9 @@ PRODUCT_PACKAGES += \
     vendor.lineage.camera.motor@1.0 \
     vendor.lineage.camera.motor@1.0-service.oneplus_msmnile \
     OnePlusCameraHelper
+
+# Characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
 
 # Device init scripts
 PRODUCT_PACKAGES += \
