f75f9b0e7a9e80ec4c1fb4f8837140d4f6856a3a
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-04 17:48:11 +0900

    guacamole: enable automatic refresh rate switching
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: I6de04f23d0ad54926a6ad7dff705fd905e4b0c59

--- device.mk
+++ device.mk
@@ -29,6 +29,20 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_PACKAGES += \
     fstab.qcom
 
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.early_phase_offset_ns=6100000 \
+    debug.sf.early_gl_phase_offset_ns=9000000 \
+    debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000 \
+    ro.surface_flinger.set_idle_timer_ms=500 \
+    ro.surface_flinger.set_touch_timer_ms=750 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_smart_90_for_video=true
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=1000000
+
 # OPFeature
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
